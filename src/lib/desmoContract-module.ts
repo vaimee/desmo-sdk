658d89f05298a0bfc3c573ef8ea8b48585b6ebc0
Brenno Tondato <brenno.tondato@gmail.com>
2022-07-07 14:53:19 -0300

    init DesmoContractIExec class

--- src/lib/desmoContract-module.ts
+++ src/lib/desmoContract-module.ts
@@ -4,4 +4,24 @@
  * exporting your lib modules from the ./src/index.ts entrypoint.
 */
 
-export const a = 1;
+import { ethers } from 'ethers';
+import { IExec, utils } from 'iexec';
+export class DesmoContractIexec {
+
+    constructor(private rpcUrl: string) {
+        const iexec = new IExec({ ethProvider: rpcUrl });
+    }
+
+    public async buyQuery(){
+        // Must trigger the iExec platform
+    }
+
+    public async getQueryResult(){
+        // Must get the result from chain
+    }
+
+    public async verifyDealContractAddress(){
+        // Must get the Id from the task and confront with the one configured. 
+    }
+
+}
