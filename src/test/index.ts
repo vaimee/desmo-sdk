fe056853e84886a9c5edf69a2ae517f05a6a9801
LudovicoGranata <Ludogranata@gmail.com>
2022-07-11 11:34:34 +0200

    adding tests for desmo-hub

--- src/test/index.ts
+++ src/test/index.ts
@@ -9,11 +9,16 @@
  * section of your package.json file)
  */
 
-import {expect} from "chai";
+import { expect } from "chai";
 import fs from "fs-extra";
+import { Subscription } from "rxjs";
+import { DesmoHub } from "..";
 
 const sandboxRoot = "./sandbox";
 const samplesRoot = "./samples";
+const infuraURL = ""; // Replace with your own Infura URL
+const privateKEY = ""; // Replace with your own private key
+const MYTDD = ""; // Replace with your own TDD for tests
 
 /**
  * Clone any files in a "./samples" folder into
@@ -24,7 +29,7 @@ const samplesRoot = "./samples";
  * reset back to an original state prior to running a test.
  */
 function resetSandbox() {
-  if(!fs.existsSync(samplesRoot)){
+  if (!fs.existsSync(samplesRoot)) {
     // Then no samples exist, and no sandbox needed
     return;
   }
@@ -35,19 +40,105 @@ function resetSandbox() {
 
 describe("Test Suite", function () {
 
-  before(function(){
+  before(function () {
     resetSandbox();
   });
 
   describe("Test Group", function () {
     it("can do something", function () {
       resetSandbox();
-      expect(false).to.be.true;
+      expect(true).to.be.true;
     });
   });
 
-  after(function(){
+  /***TESTS FOR THE DESMO-HUB***/
+
+  describe('DesmoHub Tests', function () {
+    const desmohub: DesmoHub = new DesmoHub(infuraURL);
+    desmohub.signInWithPrivateKey(privateKEY); //rembember to delete if you push to github
+    const myTDD = MYTDD;
+    
+    //start all listeners
+    desmohub.startListeners();
+
+    describe('Retrieve', function () {
+      it('should retrieve a tdd', function (done) {
+        desmohub.getTDD();
+        const subscription: Subscription = desmohub.tddRetrieval$.subscribe((event) => {
+          expect(event.url).to.equal(myTDD);
+          done();
+          //desmohub.stopListeners();
+          subscription.unsubscribe();
+        });
+
+      });
+
+    });
+
+    // disable
+    describe('Disable', function () {
+      it('should disable a tdd', function (done) {
+
+        //desmohub.startListeners();
+        desmohub.disableTDD();
+        const subscription: Subscription = desmohub.tddDisabled$.subscribe((event) => {
+          expect(event.url).to.equal(myTDD);
+          done();
+          //desmohub.stopListeners();
+          subscription.unsubscribe();
+        });
+      });
+
+      // retrieve tdd after enable
+
+      it('should have a disabled tdd', function (done) {
+
+        //desmohub.startListeners();
+        desmohub.getTDD();
+        const subscription: Subscription = desmohub.tddRetrieval$.subscribe((event) => {
+          expect(event.disabled).to.be.true;
+          done();
+          //desmohub.stopListeners();
+          subscription.unsubscribe();
+        });
+
+      });
+    });
+
+
+    //enable
+
+    describe('Enable', function () {
+      it('should enable a tdd', function (done) {
+
+        desmohub.enableTDD();
+        const subscription: Subscription = desmohub.tddEnabled$.subscribe((event) => {
+          expect(event.url).to.equal(myTDD);
+          done();
+          subscription.unsubscribe();
+
+        });
+      });
+      // retrieve tdd after enable
+      it('should have an enabled tdd', function (done) {
+
+        desmohub.getTDD();
+        const subscription: Subscription = desmohub.tddRetrieval$.subscribe((event) => {
+          expect(event.disabled).to.be.false;
+          done();
+          subscription.unsubscribe();
+
+        });
+      });
+
+    });
+  });
+
+
+  after(function () {
     resetSandbox();
   });
 
 });
+
+
