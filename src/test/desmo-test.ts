f2ab215ee2af9266608de3e99f717c2d43bce036
reluc <relu.cri@gmail.com>
2022-11-03 10:10:30 +0100

    fix(test): query interface

--- src/test/desmo-test.ts
+++ src/test/desmo-test.ts
@@ -1,4 +1,4 @@
-import { Desmo } from '@/desmo-module';
+import { Desmo, Query } from '@/desmo-module';
 import { DesmoHub } from '@/desmoHub-module';
 import { WalletSignerJsonRpc } from '@/walletSigner/walletSignerJsonRpc-module';
 import { QueryResultTypes } from '@/utils/decoder';
@@ -19,8 +19,23 @@ async function query(
 ): Promise<ReturnType<Desmo['getQueryResult']>> {
   const requestID = await desmo.generateNewRequestID();
 
-  const query =
-    '{__!_prefixList__!_:[{__!_abbreviation__!_:__!_desmo__!_,__!_completeURI__!_:__!_https://desmo.vaimee.it/__!_},{__!_abbreviation__!_:__!_qudt__!_,__!_completeURI__!_:__!_http://qudt.org/schema/qudt/__!_},{__!_abbreviation__!_:__!_xsd__!_,__!_completeURI__!_:__!_http://www.w3.org/2001/XMLSchema/__!_},{__!_abbreviation__!_:__!_monas__!_,__!_completeURI__!_:__!_https://pod.dasibreaker.vaimee.it/monas/__!_}],__!_property__!_:{__!_identifier__!_:__!_value__!_,__!_unit__!_:__!_qudt:DEG_C__!_,__!_datatype__!_:1},__!_staticFilter__!_:__!_$[?(@[--#-type--#-]==--#-Sensor--#-)]__!_}';
+  const query: Query = {
+    prefixList: [
+      { abbreviation: 'desmo', completeURI: 'https://desmo.vaimee.it/' },
+      { abbreviation: 'qudt', completeURI: 'http://qudt.org/schema/qudt/' },
+      { abbreviation: 'xsd', completeURI: 'http://www.w3.org/2001/XMLSchema/' },
+      {
+        abbreviation: 'monas',
+        completeURI: 'https://pod.dasibreaker.vaimee.it/monas/',
+      },
+    ],
+    property: {
+      identifier: 'value',
+      unit: 'qudt:DEG_C',
+      datatype: 1,
+    },
+    staticFilter: "$[?(@['type']=='Sensor')]",
+  };
   await desmo.buyQuery(
     requestID,
     query,
